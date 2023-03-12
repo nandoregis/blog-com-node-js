4e34d9d9bc42ccb63710230aca45945cf5a7dad4
nandoregis <nando.1regis@gmail.com>
2023-03-11 21:42:52 -0300

    Finalização do projeto

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const app = express();
 
 const Posts = require('./modulos/Posts.js');
 
-
 app.use( bodyParser.json() );
 app.use( bodyParser.urlencoded({ extended: true }));
 
@@ -17,36 +16,55 @@ app.use('/public', express.static(path.join(__dirname, 'public') ) );
 app.set('views', path.join(__dirname, '/pages'));
 /*-------------------------------------------------------*/
 
-
-
-const posts = async (res) => {
-    const posts = new Posts;
-    const data = (await posts.get()).data ;
-    const arr = [];
-
-    for await (const pet of data) {
-        arr.push(pet);
-    }
-    
-    res.render('home', { posts: arr});
-
-}
-
-
+const posts = new Posts;
 
 app.get('/', (req, res) => {
 
    if(!req.query.buscar) {
-    posts(res);
-   }else {
-        res.render('busca', {busca: req.query.buscar });
 
+    ( async ()=>{
+
+        const MY_POSTS = await posts.getAllPosts();
+        const POSTS_TOP = await posts.getAllPostsTop();
+        // const data = [];
+
+        if(MY_POSTS.length > 0) {
+            res.render('home', { posts : MY_POSTS, postsTop : POSTS_TOP });
+        }else {
+            res.render('erro', {});
+        }
+
+    })();
+
+   }else {
+        ( async () => {
+
+            let buscar = req.query.buscar;
+            const PESQUISA = await posts.search(buscar);
+            res.render('busca', {busca: buscar, resultado : PESQUISA });
+        })();
    }
 
 });
 
 app.get('/:slug', (req, res)=> {
-    res.render('single', {});
+    
+    (async ()=> {
+
+        let slug = req.params.slug;
+
+        const data = await posts.getPost(slug);
+        const POSTS_TOP = await posts.getAllPostsTop();
+
+        if(data) {
+            res.render('single', {post: data, postsTop : POSTS_TOP});
+        }else {
+            res.render('erro', {});
+        }
+
+    })()
+  
+
 });
 
 
--- modulos/Posts.js
+++ modulos/Posts.js
@@ -1,50 +1,84 @@
 const CLIENTE = require('./Client.js');
 
-// const POSTS = async () => {
-
-//     try {
-//         const database = CLIENT.db('portal-noticia');
-//         const posts = database.collection('posts');
-
-//         const post = posts.find();
-        
-//         return post;
-        
-//     } finally {
-//         await client.close();
-//     }
-
-// }
-
 class Posts {
 
     constructor() {
         this.cliente = CLIENTE;
-        this.query;
+        this.query = {};
         this.order;
     }
 
-    async db() {
+    async getAllPosts() {
         try {
-            const database = this.cliente.db('portal-noticia');
-            const posts = database.collection('posts');
-    
-            const postCursor = posts.find();
+            const posts = this.conexao();
+            const postCursor = posts.find(this.query, { sort : {_id : -1}});
+            const data = await this.organizationAndDataReturn(postCursor);
             
-            return postCursor;
-            
+            return data;
+
         }catch(e) {
             console.log(e);
         }
     }
 
-    async get() {
-        return  {
-            data: await this.db().then()
+    async getPost(slugValue) {
+
+        try {
+            const post = this.conexao();
+            const cursor = await post.findOne({ slug : slugValue });
+            await post.updateOne( { slug : slugValue }, { $inc : {views : 1}});
+            return cursor;
+
+        }catch(e) {
+            console.log(e)
         }
 
     }
 
+    async getAllPostsTop() {
+        try {
+            const posts = this.conexao();
+            const postCursor = posts.find(this.query, { sort : { views: -1}}).limit(2);
+            const data = await this.organizationAndDataReturn(postCursor);
+            
+            return data;
+
+        }catch(e) {
+            console.log(e);
+        }
+    }
+
+    async search(query) {
+        try {
+            const posts = this.conexao();
+            const postCursor = posts.find({titulo : {$regex : query}}, { sort : {_id: -1}});
+            const data = await this.organizationAndDataReturn(postCursor);
+            
+            return data;
+
+        }catch(e) {
+            console.log(e);
+        }
+    }
+
+    conexao() {
+        const database = this.cliente.db('portal-noticia');
+        return database.collection('posts');
+    }
+
+    async organizationAndDataReturn(arr = []) {
+        if(arr.length === 0) return arr;
+
+        const data = [];
+
+        for await (const post of arr) {
+            data.push(post);
+        }
+
+        return data;
+
+    }
+ 
 }
 
 module.exports = Posts;
